756e1b0e02e09b07a9d6b180d9a1a23684e21800
Tiago-C-Brandao <tiago_curvelo@hotmail.com>
2025-10-21 22:42:59 -0300

    feat: override instalaraplicativo method and add new method

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,37 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria) { }
+        
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            if(nomeApp.EndsWith(".ipa"))
+            {
+                Console.WriteLine($"\nInstalando o aplicativo {nomeApp} no ios");
+                Aplicativos.Add(nomeApp);
+                Console.WriteLine($"{nomeApp} instalado com sucesso!");
+            }
+            else
+            {
+                throw new Exception("Formato não suportado pelo sistema");
+            }
+        }
+
+        public override void DesinstalarAplicativo(string nomeApp)
+        {
+            bool validandoApp = Aplicativos.Contains(nomeApp);
+            if (validandoApp)
+            {
+                Console.WriteLine($"\nDesinstalando o aplicativo {nomeApp} no ios");
+                Aplicativos.Remove(nomeApp);
+                Console.WriteLine($"{nomeApp} desinstalado com sucesso!");
+            }
+            else
+            {
+                Console.WriteLine($"\n{nomeApp} não encontrado.");
+            }
+        }
     }
 }
